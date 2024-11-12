e6cd94d0e296ce9ae6c4a37aea023abf326936a8
Joel Allan <JAllan@env.sa.gov.au>
2024-11-13 10:28:01 +1030

    Added ability to split aoi into multiple downloads for > 50M records. Closes #11

--- R/get_galah.R
+++ R/get_galah.R
@@ -32,178 +32,261 @@
 #' @export
 #'
 #' @example inst/examples/get_data_ex.R
-  get_galah <- function(aoi = NULL
-                        , save_dir = NULL
-                        , get_new = FALSE
-                        , name = "galah"
-                        , data_map = NULL
-                        , node = "ALA"
-                        , qry = NULL
-                        , check_rel_metres = TRUE
-                        , filter_inconsistent = TRUE
-                        , ...
-                        ) {
+get_galah <- function(aoi = NULL
+                      , save_dir = NULL
+                      , get_new = FALSE
+                      , name = "galah"
+                      , data_map = NULL
+                      , node = "ALA"
+                      , qry = NULL
+                      , check_rel_metres = TRUE
+                      , filter_inconsistent = TRUE
+                      , ...
+) {
 
-    # save file -------
-    save_file <- file_prep(save_dir, name, ...)
+  # save file -------
+  save_file <- file_prep(save_dir, name, ...)
 
-    # run the qry ---------
-    get_new <- if(!file.exists(save_file)) TRUE else get_new
+  # run the qry ---------
+  get_new <- if(!file.exists(save_file)) TRUE else get_new
 
-    if(get_new) {
+  if(get_new) {
 
-      # atlas -------
-      old_atlas <- galah::galah_config()$atlas$region
-      galah::galah_config(atlas = node)
-      on.exit(galah::galah_config(atlas = old_atlas))
+    # atlas -------
+    old_atlas <- galah::galah_config()$atlas$region
+    galah::galah_config(atlas = node)
+    on.exit(galah::galah_config(atlas = old_atlas))
 
-      # qry -------
-      ## initiate-------
-      if(is.null(qry)) {
+    # qry -------
+    ## initiate-------
+    if(is.null(qry)) {
 
-        qry <- galah::galah_call()
+      qry <- galah::galah_call()
 
-      }
+    }
 
-      ## aoi------
-      if(!is.null(aoi)) qry <- qry %>%
-          galah::galah_geolocate(aoi)
+    ## select-------
+    if(is.null(data_map)) {
+
+      get_groups <- c("basic", "event", "taxonomy")
+
+      qry <- qry %>%
+        galah::galah_select(group = get_groups)
+
+      data_map <- tibble::tibble(col = unique(unlist(purrr::map(get_groups, galah:::preset_groups)))) %>%
+        dplyr::mutate(!!rlang::ensym(name) := col)
+
+    } else {
+
+      select_names <- choose_names(data_map = data_map
+                                   , this_name = name
+      ) %>%
+        dplyr::filter(value %in% galah::show_all("fields")$id)
+
+      qry <- qry %>%
+        galah::select(recordID # inc recordID here: see https://github.com/AtlasOfLivingAustralia/galah-R/issues/239
+                      , select_names$value
+        )
+
+    }
+
+    ## aoi------
+    if(!is.null(aoi)) qry1 <- qry %>%
+      galah::galah_geolocate(aoi)
 
 
-      ## check records--------
-      records <- qry %>%
-        galah::atlas_counts()
+    ## check records--------
+    records <- qry1 %>%
+      galah::atlas_counts() %>%
+      dplyr::pull(count)
 
-      if(records > 50000000) {
+    if(records > 50000000 & !is.null(aoi)) {
 
-        stop("Returned records ("
-             , records
-             , ") would exceed limit of 50 million"
-             )
+      ### split aoi if records > 50M ----
 
-      }
+      scalar <- celing(records/50000000)+1
+
+      combos <- tibble::tibble(cells_x = seq(1,3)
+                               , cells_y = seq(1,3)
+      ) %>%
+        expand.grid() %>%
+        dplyr::mutate(tot = cells_x + cells_y
+                      , diff = abs(cells_x - cells_y)
+                      , prop_diff = round(diff/tot*100,2)
+        ) %>%
+        dplyr::arrange(tot,prop_diff)
+
+      grd <- purrr::map2(combos$cells_x
+                          , combos$cells_y
+                          , \(x,y)
+
+                          aoi %>%
+                            sf::st_make_grid(n = c(x,y)) %>%
+                            sf::st_sf() %>%
+                            dplyr::mutate(cell = dplyr::row_number()) %>%
+                            tidyr::nest(data = -(cell)) %>%
+                            dplyr::mutate(rec = purrr::map_int(data, \(a) qry %>%
+                                                                 galah::galah_geolocate(a) %>%
+                                                                 galah::atlas_counts() %>%
+                                                                 dplyr::pull(count)
+                            )
+                            , combo = paste0(x, " x ", y)
+                            , cells_x = x
+                            , cells_y = y
+                            , cells = x + y
+                            , above_limit = rec > 50000000
+                            ) %>%
+                            tidyr::unnest(cols = c(data))
+
+      ) %>%
+        purrr::list_rbind() %>%
+        dplyr::group_by(combo) %>%
+        dplyr::mutate(any_above_limit = any(above_limit)) %>%
+        dplyr::ungroup() %>%
+        dplyr::filter(!any_above_limit) %>%
+        dplyr::filter(cells == min(cells)) %>%
+        dplyr::filter(cells_x == min(cells_x)) %>%
+        dplyr::filter(cells_y == min(cells_y)) %>%
+        sf::st_sf()
+
+      qry <- grd %>%
+        dplyr::select(cell, geometry) %>%
+        tidyr::nest(data = -c(cell)) %>%
+        .$data %>%
+        purrr::map(\(x) qry %>%
+                          galah::galah_geolocate(x)
+      )
+
+
+      warning("Returned records for query/aoi ("
+              , records
+              , ") would exceed limit of 50 million. "
+              , "aoi has been split into a "
+              , unique(grd$combo)
+              , " cell grid to allow multiple smaller downloads."
+      )
+
+    } else if(records > 50000000 & is.null(aoi)) {
+
+      ### stop if no aoi & records > 50M ----
+
+      stop("Returned records for query ("
+           , records
+           , ") would exceed limit of 50 million"
+      )
+
+    } else {
+
+      ### continue if records < 50M ----
+
+      qry <- qry1
 
       message(records
               , " records available from galah via "
               , galah::galah_config()$atlas$acronym
               , " node"
-              )
-
-      ## select-------
-      if(is.null(data_map)) {
-
-        get_groups <- c("basic", "event", "taxonomy")
-
-        qry <- qry %>%
-          galah::galah_select(group = get_groups)
-
-        data_map <- tibble::tibble(col = unique(unlist(purrr::map(get_groups, galah:::preset_groups)))) %>%
-          dplyr::mutate(!!rlang::ensym(name) := col)
-
-      } else {
-
-        select_names <- choose_names(data_map = data_map
-                                     , this_name = name
-                                     ) %>%
-          dplyr::filter(value %in% galah::show_all("fields")$id)
-
-        qry <- qry %>%
-          galah::select(recordID # inc recordID here: see https://github.com/AtlasOfLivingAustralia/galah-R/issues/239
-                        , select_names$value
-                        )
-
-      }
-
-      make_doi <- galah::galah_config()$user$download_reason_id != 10
-
-      ## retrieve ------
-      temp <- qry %>%
-        galah::atlas_occurrences(mint_doi = make_doi)
-
-      ## rel_metres-------
-      if(check_rel_metres) {
-
-        temp <- temp %>%
-          dplyr::rename(cuim = coordinateUncertaintyInMeters
-                        , gim = generalisationInMetres
-                        ) %>%
-          dplyr::mutate(coordinateUncertaintyInMeters = dplyr::case_when(!is.na(gim) & gim > cuim ~ gim
-                                                                         , TRUE ~ cuim
-                                                                         )
-                        )
-
-      }
-
-      ## filter_inconsistent --------
-      if(filter_inconsistent) {
-
-        temp <- temp %>%
-            dplyr::filter(!(occurrenceStatus == "ABSENT" &
-                              !is.na(organismQuantity) &
-                              organismQuantity > 0
-                            )
-                          ) %>%
-            dplyr::filter(!(occurrenceStatus == "PRESENT" &
-                            !is.na(organismQuantity) &
-                            organismQuantity == 0
-                            )
-                          )
-
-      }
-
-
-      # bib -------
-      if(make_doi) {
-
-        doi <- attr(temp, "doi")
-
-        if(is.character(doi)) {
-
-          bib <- bibentry(bibtype = "MISC"
-                          , key = "galah"
-                          , title = "Occurrence download data"
-                          , author = utils::person("Atlas Of Living Australia")
-                          , publisher = "Atlas Of Living Australia"
-                          , year = base::format(base::Sys.Date(), "%Y")
-                          , doi = fs::path(basename(dirname(doi)), basename(doi))
-                          ) %>%
-            utils::toBibtex()
-
-          readr::write_lines(bib
-                             , file = fs::path(dirname(save_file)
-                                               , paste0(basename(dirname(save_file)), ".bib")
-                                               )
-                             , append = TRUE
-                             )
-
-        } else {
-
-          message("problem with doi: galah entry not written to .bib")
-
-        }
-
-      }
-
-      # remap ------
-      temp <- remap_data_names(this_name = name
-                               , df_to_remap = temp
-                               , data_map = data_map
-                               , out_file = save_file
-                               , final_select = TRUE
-                               , final_select_col = "bio_all"
-                               , ...
-                               )
-
-    } else {
-
-      temp <- rio::import(save_file
-                          , setclass = "tibble"
-                          )
+      )
 
     }
 
-    return(temp)
+    ## retrieve ------
+
+    make_doi <- galah::galah_config()$user$download_reason_id != 10
+
+    temp <- qry %>%
+      purrr::map(\(x) x %>%
+                   galah::atlas_occurrences(mint_doi = make_doi)
+      )
+
+    # doi string for use below
+    if(make_doi) doi <- purrr::map_chr(temp, \(x) attr(x, "doi"))
+
+    # combine multiple downloads
+    temp <- temp %>%
+      purrr::list_rbind()
+
+    ## rel_metres-------
+    if(check_rel_metres) {
+
+      temp <- temp %>%
+        dplyr::rename(cuim = coordinateUncertaintyInMeters
+                      , gim = generalisationInMetres
+        ) %>%
+        dplyr::mutate(coordinateUncertaintyInMeters = dplyr::case_when(!is.na(gim) & gim > cuim ~ gim
+                                                                       , TRUE ~ cuim
+        )
+        )
+
+    }
+
+    ## filter_inconsistent --------
+    if(filter_inconsistent) {
+
+      temp <- temp %>%
+        dplyr::filter(!(occurrenceStatus == "ABSENT" &
+                          !is.na(organismQuantity) &
+                          organismQuantity > 0
+        )
+        ) %>%
+        dplyr::filter(!(occurrenceStatus == "PRESENT" &
+                          !is.na(organismQuantity) &
+                          organismQuantity == 0
+        )
+        )
+
+    }
+
+
+    # bib -------
+    if(make_doi) {
+
+      if(is.character(doi)) {
+
+        bib <- bibentry(bibtype = "MISC"
+                        , key = "galah"
+                        , title = "Occurrence download data"
+                        , author = utils::person("Atlas Of Living Australia")
+                        , publisher = "Atlas Of Living Australia"
+                        , year = base::format(base::Sys.Date(), "%Y")
+                        , doi = paste(fs::path(basename(dirname(doi)), basename(doi)), collapse = "; ")
+        ) %>%
+          utils::toBibtex()
+
+        readr::write_lines(bib
+                           , file = fs::path(dirname(save_file)
+                                             , paste0(basename(dirname(save_file)), ".bib")
+                           )
+                           , append = TRUE
+        )
+
+      } else {
+
+        message("problem with doi: galah entry not written to .bib")
+
+      }
+
+    }
+
+    # remap ------
+    temp <- remap_data_names(this_name = name
+                             , df_to_remap = temp
+                             , data_map = data_map
+                             , out_file = save_file
+                             , final_select = TRUE
+                             , final_select_col = "bio_all"
+                             , ...
+    )
+
+  } else {
+
+    temp <- rio::import(save_file
+                        , setclass = "tibble"
+    )
 
   }
+
+  return(temp)
+
+}
 
 
